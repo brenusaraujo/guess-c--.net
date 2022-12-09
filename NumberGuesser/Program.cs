51566e8d6c8dd8817a537a04cc268701f9bc8c33
Breno Araujo <brenoaraujo50@icloud.com>
2022-12-09 12:39:00 +0000

    adiciona novo arquivo

---
+++ NumberGuesser/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+// namespace 
+namespace NumberGuesser
+{
+    //main class
+    class Program
+    {
+        //entrypoint method
+        static void Main(string[] args)
+        {
+            //start here//
+            Console.Write("teste");
+        }
+    }
+}
